c2ea3cd6e280ee81845c46235ae8b04be2d47301
chenjiankun <chenjiankun1@huawei.com>
2016-09-08 00:51:35 +0000

    none

--- plugin/ycm/install.sh
+++ plugin/ycm/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y vim
-sudo apt-get install -y cmake
+sudo apt-get install -y build-essential cmake
 sudo apt-get install -y python-dev python3-dev
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
