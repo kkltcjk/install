695a36a667982405079c175ed1322ba4fc28d095
chenjiankun <chenjiankun1@huawei.com>
2016-09-14 13:03:07 +0000

    none

--- plugin/oh-my-zsh/install.sh
+++ plugin/oh-my-zsh/install.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-sudo apt-get install git
-sudo apt-get install zsh
+sudo apt-get install -y curl
+sudo apt-get install -y git
+sudo apt-get install -y zsh
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
