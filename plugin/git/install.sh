8923833545be574f54f6fd7e5911a1b820b13d1f
chenjiankun <chenjiankun1@huawei.com>
2016-12-21 10:42:37 +0000

    add git config

---
+++ plugin/git/install.sh
@@ -0,0 +1,9 @@
+apt-get install -y git
+apt-get install -y git-review
+apt-get install -y vim
+
+git config --global user.name 'chenjiankun'
+git config --global user.emial 'chenjiankun1@huawei.com'
+git config --global gitreview.username 'chenjiankun'
+git config --global gitreview.email 'chenjiankun1@huawei.com'
+git config --global core.editor 'vim'
