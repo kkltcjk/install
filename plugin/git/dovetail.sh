100bad00484313d9e3d1bf4120bebb4c7178fceb
chenjiankun <chenjiankun1@huawei.com>
2017-10-28 02:22:20 +0000

    Add git message for dovetail
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

---
+++ plugin/git/dovetail.sh
@@ -0,0 +1,9 @@
+apt-get install -y git
+apt-get install -y git-review
+apt-get install -y vim
+
+git config --global user.name 'grakiss'
+git config --global user.email 'grakiss.wanglei@huawei.com'
+git config --global gitreview.username 'grakiss'
+git config --global gitreview.email 'grakiss.wanglei@huawei.com'
+git config --global core.editor 'vim'
